25d73666d679bea215669728f6c84a1328b45293
bahrus <anderson.bruce.b@gmail.com>
2019-11-21 07:08:22 -0500

    ...

---
+++ test/test-req.js
@@ -0,0 +1,20 @@
+const xt = require('xtal-test/index');
+const test = require('tape');
+async function customTests(page) {
+    await page.waitFor(4000);
+    const errorTags = await page.$$('[err=true]');
+    const markings = await page.$$('[mark]');
+    const TapeTestRunner = {
+        test: test
+    };
+    TapeTestRunner.test('testing dev.html', (t) => {
+        t.equal(errorTags.length, 0);
+        t.equals(markings.length, 1);
+        t.end();
+    });
+}
+(async () => {
+    await xt.runTests({
+        path: 'test/fly-req.html'
+    }, customTests);
+})();
