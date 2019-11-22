682d80b7c1961301f5aac03b562821f87ec24c14
bahrus <anderson.bruce.b@gmail.com>
2019-11-22 06:07:54 -0500

    ...

--- test/test-all.js
+++ test/test-all.js
@@ -1,13 +1,19 @@
 const xt = require('xtal-test/index');
 const test = require('tape');
-async function customTests(page) {
+async function customTestsGet(page) {
+    await customTests(page, 1, 'test/fly-get.html');
+}
+async function customTestsReq(page) {
+    await customTests(page, 1, 'test/fly-req.html');
+}
+async function customTests(page, noOfExpectedMarkings, path) {
     await page.waitFor(4000);
     const errorTags = await page.$$('[err=true]');
     const markings = await page.$$('[mark]');
     const TapeTestRunner = {
         test: test
     };
-    TapeTestRunner.test('testing fly-req.html', (t) => {
+    TapeTestRunner.test(`testing ${path}`, (t) => {
         t.equal(errorTags.length, 0);
         t.equals(markings.length, 1);
         t.end();
@@ -15,6 +21,9 @@ async function customTests(page) {
 }
 (async () => {
     await xt.runTests({
+        path: 'test/fly-get.html'
+    }, customTestsGet);
+    await xt.runTests({
         path: 'test/fly-req.html'
-    }, customTests);
+    }, customTestsReq);
 })();
--- test/test-all.ts
+++ test/test-all.ts
@@ -3,24 +3,35 @@ const xt = require('xtal-test/index') as IXtalTestRunner;
 const test = require('tape');
 import { Page } from "puppeteer"; //typescript
 import { Test } from "tape";
-async function customTests(page: Page) {
+
+async function customTestsGet(page: Page) {
+    await customTests(page, 1, 'test/fly-get.html');
+}
+
+async function customTestsReq(page: Page) {
+    await customTests(page, 1, 'test/fly-req.html');
+}
+
+async function customTests(page: Page, noOfExpectedMarkings: number, path: string){
     await page.waitFor(4000);
     const errorTags = await page.$$('[err=true]');
     const markings = await page.$$('[mark]');
     const TapeTestRunner = {
         test: test
     } as Test;
-    TapeTestRunner.test('testing fly-get.html', (t: any) => {
+    TapeTestRunner.test(`testing ${path}`, (t: any) => {
         t.equal(errorTags.length, 0);
         t.equals(markings.length, 1);
         t.end();
     });
-
 }
 
 (async () => {
     await xt.runTests({
         path: 'test/fly-get.html'
-    }, customTests);
+    }, customTestsGet);
+    await xt.runTests({
+        path: 'test/fly-req.html'
+    }, customTestsReq);
 })();
 
