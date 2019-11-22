5cd0f6e7215409e1c596b6a05742a186d8365acc
bahrus <anderson.bruce.b@gmail.com>
2019-11-22 10:12:53 -0500

    ...

--- test/test-all.js
+++ test/test-all.js
@@ -1,29 +1,28 @@
 const xt = require('xtal-test/index');
-async function customTestsGet(page) {
-    await customTests(page, 1, 'test/fly-get.html', false);
-}
-async function customTestsReq(page) {
-    await customTests(page, 1, 'test/fly-req.html', true);
-}
-async function customTests(page, noOfExpectedMarkings, path, end) {
-    await page.waitFor(4000);
-    const errorTags = await page.$$('[err=true]');
-    if (errorTags.length > 0)
-        throw 'Found tag with attribute err=true';
-    const markings = await page.$$('[mark]');
-    if (markings.length !== noOfExpectedMarkings) {
-        throw "Found " + markings.length + " tags with attribute mark.  Expecting " + noOfExpectedMarkings;
-    }
-}
+// async function customTestsGet(page: Page) {
+//     await customTests(page, 1, 'test/fly-get.html', false);
+// }
+// async function customTestsReq(page: Page) {
+//     await customTests(page, 1, 'test/fly-req.html', true);
+// }
+// async function customTests(page: Page, noOfExpectedMarkings: number, path: string, end: boolean){
+//     await page.waitFor(4000);
+//     const errorTags = await page.$$('[err=true]');
+//     if(errorTags.length > 0) throw 'Found tag with attribute err=true';
+//     const markings = await page.$$('[mark]');
+//     if(markings.length !== noOfExpectedMarkings){
+//         throw "Found " + markings.length + " tags with attribute mark.  Expecting " + noOfExpectedMarkings
+//     }
+// }
 (async () => {
     await xt.runTests([
         {
             path: 'test/fly-get.html',
-            customTest: customTestsGet
+            expectedNoOfSuccessMarkers: 1,
         },
         {
             path: 'test/fly-req.html',
-            customTest: customTestsReq
+            expectedNoOfSuccessMarkers: 1,
         }
     ]);
     console.log("Tests Passed.  Have a nice day.");
