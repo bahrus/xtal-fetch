a2b5f810fc26013c716a7c8093983a0f8e19a677
bahrus <anderson.bruce.b@gmail.com>
2019-11-22 10:14:46 -0500

    ...

--- test/test-all.js
+++ test/test-all.js
@@ -1,19 +1,4 @@
 const xt = require('xtal-test/index');
-// async function customTestsGet(page: Page) {
-//     await customTests(page, 1, 'test/fly-get.html', false);
-// }
-// async function customTestsReq(page: Page) {
-//     await customTests(page, 1, 'test/fly-req.html', true);
-// }
-// async function customTests(page: Page, noOfExpectedMarkings: number, path: string, end: boolean){
-//     await page.waitFor(4000);
-//     const errorTags = await page.$$('[err=true]');
-//     if(errorTags.length > 0) throw 'Found tag with attribute err=true';
-//     const markings = await page.$$('[mark]');
-//     if(markings.length !== noOfExpectedMarkings){
-//         throw "Found " + markings.length + " tags with attribute mark.  Expecting " + noOfExpectedMarkings
-//     }
-// }
 (async () => {
     await xt.runTests([
         {
--- test/test-all.ts
+++ test/test-all.ts
@@ -1,25 +1,6 @@
 import { IXtalTestRunner, IXtalTestRunnerOptions } from 'xtal-test/index.js';
 const xt = require('xtal-test/index') as IXtalTestRunner;
-import { Page } from "puppeteer"; //typescript
 
-
-// async function customTestsGet(page: Page) {
-//     await customTests(page, 1, 'test/fly-get.html', false);
-// }
-
-// async function customTestsReq(page: Page) {
-//     await customTests(page, 1, 'test/fly-req.html', true);
-// }
-
-// async function customTests(page: Page, noOfExpectedMarkings: number, path: string, end: boolean){
-//     await page.waitFor(4000);
-//     const errorTags = await page.$$('[err=true]');
-//     if(errorTags.length > 0) throw 'Found tag with attribute err=true';
-//     const markings = await page.$$('[mark]');
-//     if(markings.length !== noOfExpectedMarkings){
-//         throw "Found " + markings.length + " tags with attribute mark.  Expecting " + noOfExpectedMarkings
-//     }
-// }
 
 (async () => {
     await xt.runTests([
